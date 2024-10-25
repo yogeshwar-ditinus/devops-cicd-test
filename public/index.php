139da33ddf264dc2f0f07ba38e807db25a70984d
test dev <devinder@ditinustechnology.com>
2024-10-25 17:04:50 +0000

    2326

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance 232
+| Check If The Application Is Under Maintenance 232 232
 |--------------------------------------------------------------------------
 |
 | If the application is in maintenance / demo mode via the "down" command
